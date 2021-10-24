eae12783f3c2a4cfeab03c0b56108420cd99c902
roosWe <rooswensveen@hotmail.com>
2021-10-24 16:04:05 +0200

    New features
    Added sound, lasers --> candies

--- Assets/02_Student Folders/RoosWensveen_Assets/Scripts/RemoveBlockage.cs
+++ Assets/02_Student Folders/RoosWensveen_Assets/Scripts/RemoveBlockage.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class RemoveBlockage : MonoBehaviour
 {
-
     // Update is called once per frame
     void Update()
     {
         if(GameObject.FindGameObjectsWithTag("Lollipop").Length <= 0) {
         //if(Input.GetKey(KeyCode.Space)) {
             Debug.Log("Done");  
-            Destroy(gameObject);
+            Destroy(gameObject);    
         } 
     }
 }
