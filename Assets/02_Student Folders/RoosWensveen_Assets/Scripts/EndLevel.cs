a45011df851e85edb0c9385f4c0c69f5e6376633
roosWe <rooswensveen@hotmail.com>
2021-10-27 16:30:46 +0200

    -

--- Assets/02_Student Folders/RoosWensveen_Assets/Scripts/EndLevel.cs
+++ Assets/02_Student Folders/RoosWensveen_Assets/Scripts/EndLevel.cs
@@ -25,6 +25,6 @@ public class EndLevel : MonoBehaviour
 
     private IEnumerator WaitForSceneLoad() {
         yield return new WaitForSeconds(6);
-        SceneManager.LoadScene("MainMenuScene");
+        SceneManager.LoadScene("RoosWensveen_Scene");
     }
 }
